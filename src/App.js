69cc3c87e0034e39760e7fff059abe8bccc02c4c
erasmo <erasmomontes07@gmail.com>
2020-07-26 23:41:07 -0400

    Add form to create appointment.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Form from './components/Form';
 
 function App() {
+  
+  const [appointments, setAppointments] = useState([]);
+
+  const addAppointment = appointment => {
+    setAppointments([ ...appointments, appointment]);
+  }
+
   return (
     <Fragment>
       <h1>Administrador de citas</h1>
       <div className="container">
         <div className="row">
           <div className="one-half column">
-            <Form />
+            <Form addAppointment={addAppointment}/>
           </div>
           <div className="one-half column">
             Two
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,72 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import { v4 as uuid } from 'uuid';
 
-const Form = () => {
+
+const Form = ({addAppointment}) => {
+    const [appointment, setAppointment] = useState({
+        pet: '',
+        owner: '',
+        date: '',
+        time: '',
+        description: ''
+    }); 
+
+    const [error, setError] = useState(false);
+    
+    const {pet, owner, date, time, description} = appointment;
+
+    const handleChange = e => {
+        setAppointment({
+            ...appointment,
+            [e.target.name] : e.target.value
+        });
+    }
+
+    const handelSubmit = e => {
+        e.preventDefault();
+        if (pet.trim() === '' ||
+            owner.trim() === '' ||
+            date.trim() === '' ||
+            time.trim() === '' ||
+            description.trim() === ''
+        ) {
+            setError(true);
+            return;
+        } else {
+            setError(false);
+        }
+
+        appointment.id = uuid()  
+        addAppointment(appointment);
+
+        setAppointment({
+            pet: '',
+            owner: '',
+            date: '',
+            time: '',
+            description: ''
+        })
+    }
+
+
+
     return ( 
         <Fragment>
             <h1>Crear cita</h1>
 
-            <form action="">
+            {error
+            ? <p className="alert-error">Todo los campos son obligatorios</p>
+            : null}
+
+            <form onSubmit={handelSubmit}>
                 <label>Nombre mascota</label>
                 <input
                     type="text"
                     name='pet'
                     className='u-full-width'
                     placeholder='Nombre de la mascota'
+                    onChange={handleChange}
+                    value={pet}
                 />
                 <label>Nombre de dueño</label>
                 <input
@@ -19,23 +74,31 @@ const Form = () => {
                     name='owner'
                     className='u-full-width'
                     placeholder='Nombre del dueño'
+                    onChange={handleChange}
+                    value={owner}
                 />
                 <label>Fecha</label>
                 <input
                     type="date"
                     name='date'
                     className='u-full-width'
+                    onChange={handleChange}
+                    value={date}
                 />
                 <label>Hora</label>
                 <input
                     type="time"
                     name='time'
                     className='u-full-width'
+                    onChange={handleChange}
+                    value={time}
                 />
                 <label>Sintomas</label>
                 <textarea
                     name="description"
                     cols="30" rows="10"
+                    onChange={handleChange}
+                    value={description}
                     className='u-full-width'>
                 </textarea>
                 <button
